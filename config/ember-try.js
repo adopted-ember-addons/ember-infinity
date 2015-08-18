b9da6b1486dc9d5d9b8e1c439ce35627a5d111c3
Mike North <northm@yahoo-inc.com>
2015-08-18 11:44:16 -0700

    Fix ember-try config

--- config/ember-try.js
+++ config/ember-try.js
@@ -5,30 +5,55 @@ module.exports = {
       dependencies: { }
     },
     {
+      name: 'ember-1.10',
+      dependencies: {
+        ember: '~1.10.0'
+      }
+    },
+    {
+      name: 'ember-1.11',
+      dependencies: {
+        ember: '~1.11.3'
+      }
+    },
+    {
+      name: 'ember-1.12',
+      dependencies: {
+        ember: '~1.12.0'
+      }
+    },
+    {
+      name: 'ember-1.13',
+      dependencies: {
+        ember: '~1.13.0'
+      }
+    },
+    {
       name: 'ember-release',
       dependencies: {
-        'ember': 'components/ember#release'
+        ember: 'components/ember#release'
+
       },
       resolutions: {
-        'ember': 'release'
+        ember: 'release'
       }
     },
     {
       name: 'ember-beta',
       dependencies: {
-        'ember': 'components/ember#beta'
+        ember: 'components/ember#beta'
       },
       resolutions: {
-        'ember': 'beta'
+        ember: 'beta'
       }
     },
     {
       name: 'ember-canary',
       dependencies: {
-        'ember': 'components/ember#canary'
+        ember: 'components/ember#canary'
       },
       resolutions: {
-        'ember': 'canary'
+        ember: 'canary'
       }
     }
   ]
