ac1d77c968932b96938df8c85218ad8f880bdded
Ian Barrick <ianbarrick@gmail.com>
2016-05-22 16:24:00 -0400

    added store to InfiniteArray

--- addon/utils/array.js
+++ addon/utils/array.js
@@ -2,4 +2,4 @@ import Ember from 'ember';
 import DS from 'ember-data';
 import ArrayMixin from '../mixins/array';
 
-export const InfiniteArray = DS.RecordArray.extend(ArrayMixin);
+export const InfiniteArray = DS.RecordArray.extend(ArrayMixin,{store: Ember.inject.service()});
