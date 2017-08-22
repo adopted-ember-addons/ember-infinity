92393f3c424652a68398f16df97c163473542eb4
Scott Newcomer <snewcomer@users.noreply.github.com>
2017-08-21 18:05:14 -0700

    [INFINITY MODEL]: move fetch params to infinity-model (#231)
    * [INFINITY MODEL]: move fetch params to infinity-model
    
    allows users to extend InfinityModel for their own logic
    
    * update version to alpha.1

--- addon/utils.js
+++ addon/utils.js
@@ -1,3 +1,6 @@
+import Ember from 'ember';
+const { typeOf, deprecate } = Ember;
+
 export let objectAssign = Object.assign || function objectAssign(target) {
   'use strict';
   if (target == null) {
@@ -17,3 +20,32 @@ export let objectAssign = Object.assign || function objectAssign(target) {
   }
   return target;
 };
+
+/**
+ * determine param to set on infinityModel
+ * if user passes null, then don't send query param in request
+ * if user does not pass anything for value, then see if defined on route
+ * else set to default param
+ * @method typeOfCheck
+ * @param {String} value - param passed with infinityRoute
+ * @param {String} option - property defined on user route
+ * @param {String} - default
+ * @return {String} 
+ */
+export function typeOfCheck(optionParam, routeParam, defaultParam) {
+  if (typeOf(optionParam) === 'null' || typeOf(routeParam) === 'null') {
+    // allow user to set to null if passed into infinityRoute explicitly
+    return;
+
+  } else if (optionParam) {
+    return optionParam;
+
+  } else if (routeParam) {
+    deprecate(`Ember Infinity: Please migrate route param - ${routeParam} - to be explicitly passed as second argument to infinityModel`);
+    return routeParam;
+
+  } else {
+    return defaultParam;
+
+  }
+}
