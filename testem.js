a956a2aafa014609ecef910b18df31ce00ddcbc6
Scott Newcomer <snewcomer@users.noreply.github.com>
2017-08-21 18:05:14 -0700

    [INFINITY MODEL]: move fetch params to infinity-model (#231)
    * [INFINITY MODEL]: move fetch params to infinity-model
    
    allows users to extend InfinityModel for their own logic
    
    * update version to alpha.1

--- testem.js
+++ testem.js
@@ -4,10 +4,16 @@ module.exports = {
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
   "launch_in_ci": [
-    "Firefox"
+    "Chrome"
   ],
   "launch_in_dev": [
-    "Firefox",
-    "Chrome"
-  ]
+    "Chrome",
+    "Firefox"
+  ],
+  "browser_args": {
+    "Chrome": {
+      "mode": "ci",
+      "args": [ '--headless', '--disable-gpu', '--remote-debugging-port=9222' ]
+    }
+  },
 };
