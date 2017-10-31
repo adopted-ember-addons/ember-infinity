2183a873479c086bdb4482ea0e1f48120a5601d8
Scott Newcomer <snewcomer24@gmail.com>
2017-10-30 19:39:59 -0700

    downgrade to ember 2.11 and qunit locked at 3.0.1

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -3,7 +3,11 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-const App = Ember.Application.extend({
+let App;
+
+Ember.MODEL_FACTORY_INJECTIONS = true;
+
+App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
