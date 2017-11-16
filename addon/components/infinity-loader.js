eebea924ba81632414bb0352c35c98242166fe3b
snewcomer24 <snewcomer24@gmail.com>
2017-11-16 12:19:36 -0800

    cleanup code

--- addon/components/infinity-loader.js
+++ addon/components/infinity-loader.js
@@ -14,7 +14,6 @@ const InfinityLoaderComponent = Ember.Component.extend(InViewportMixin, {
   loadedText: 'Infinite Model Entirely Loaded.',
   hideOnInfinity: false,
   developmentMode: false,
-  // scrollable: null,
   triggerOffset: 0,
   isVisible: true,
 
@@ -28,10 +27,7 @@ const InfinityLoaderComponent = Ember.Component.extend(InViewportMixin, {
 
   didInsertElement() {
     this._super(...arguments);
-    // this._setupScrollable();
     this.set('guid', Ember.guidFor(this));
-    // this._bindEvent('scroll');
-    // this._bindEvent('resize');
 
     this.setProperties({
       viewportSpy: true,
@@ -42,12 +38,6 @@ const InfinityLoaderComponent = Ember.Component.extend(InViewportMixin, {
         left: 0
       }
     });
-
-    // if (this.get('_isInfinityPromiseArray')) {
-    //   this.get('infinityModel').then(() => this._loadMoreIfNeeded());
-    // } else {
-    //   this._loadMoreIfNeeded();
-    // }
   },
 
   willDestroyElement() {
@@ -58,52 +48,6 @@ const InfinityLoaderComponent = Ember.Component.extend(InViewportMixin, {
   _isInfinityPromiseArray: Ember.computed('infinityModel', function() {
     return (this.get('infinityModel') instanceof InfinityPromiseArray);
   }),
-
-  // _bindEvent(eventName) {
-  //   this.get('_scrollable').on(`${eventName}.${this.get('guid')}`, () => {
-  //     Ember.run.debounce(this, this._loadMoreIfNeeded, this.get('eventDebounce'));
-  //   });
-  // },
-
-  // _unbindEvent(eventName) {
-  //   let scrollable = this.get('_scrollable');
-  //   if (scrollable) {
-  //     scrollable.off(`${eventName}.${this.get('guid')}`);
-  //   }
-  // },
-
-  // /**
-  //  * determine how far the infinity-loader component is from the top of the
-  //   scrollable element
-  //  * @method _selfOffset
-  //  * @return {Number}
-  //  */
-  // _selfOffset() {
-  //   if (this.get('_customScrollableIsDefined')) {
-  //     return this.$().offset().top - this.get("_scrollable").offset().top + this.get("_scrollable").scrollTop();
-  //   } else {
-  //     return this.$().offset().top;
-  //   }
-  // },
-
-  // /**
-  //  * total pixels where _scrollable's bottom position is from top of screen
-  //  * @method _bottomOfScrollableOffset
-  //  * @return {Number}
-  //  */
-  // _bottomOfScrollableOffset() {
-  //   return this.get('_scrollable').height() + this.get("_scrollable").scrollTop();
-  // },
-
-  // /**
-  //  * distance infinity loader is from top of _scrollable/window's top position
-  //   less the buffer amount specified by triggerOffset
-  //  * @method _triggerOffset
-  //  * @return {Number}
-  //  */
-  // _triggerOffset() {
-  //   return this._selfOffset() - this.get('triggerOffset');
-  // },
 
   /**
    * @method didEnterViewport
@@ -126,15 +70,6 @@ const InfinityLoaderComponent = Ember.Component.extend(InViewportMixin, {
   didExitViewport() {
     this._cancelTimers();
   },
-
-  // scheduleScrolledToBottom: observer('rows.[]', 'viewportEntered', function() {
-  //   if (this.get('viewportEntered')) {
-  //     /*
-  //      Continue scheduling onScrolledToBottom until no longer in viewport
-  //      */
-  //     this._scheduleScrolledToBottom();
-  //   }
-  // }),
 
   _scheduleScrolledToBottom() {
     this._schedulerTimer = run.scheduleOnce('afterRender', this, this._debounceScrolledToBottom);
@@ -161,38 +96,6 @@ const InfinityLoaderComponent = Ember.Component.extend(InViewportMixin, {
     run.cancel(this._schedulerTimer);
     run.cancel(this._debounceTimer);
   },
-
-  // _loadMoreIfNeeded() {
-  //   const infinityModel = this.get('infinityModelContent');
-  //   if (this._shouldLoadMore() && infinityModel) {
-  //   }
-  // },
-
-  // _setupScrollable() {
-  //   var scrollable = this.get('scrollable');
-  //   if (Ember.typeOf(scrollable) === 'string') {
-  //     var items = Ember.$(scrollable);
-  //     if (items.length === 1) {
-  //       this.set('_scrollable', items.eq(0));
-  //     } else if (items.length > 1) {
-  //       throw new Ember.Error("Ember Infinity: Multiple scrollable elements found for: " + scrollable);
-  //     } else {
-  //       throw new Ember.Error("Ember Infinity: No scrollable element found for: " + scrollable);
-  //     }
-  //     this.set('_customScrollableIsDefined', true);
-  //   } else if (scrollable === undefined || scrollable === null) {
-  //     this.set('_scrollable', Ember.$(window));
-  //     this.set('_customScrollableIsDefined', false);
-  //   } else {
-  //     throw new Ember.Error("Ember Infinity: Scrollable must either be a css selector string or left empty to default to window");
-  //   }
-  // },
-
-  // loadedStatusDidChange: Ember.observer('infinityModelContent.reachedInfinity', 'destroyOnInfinity', function () {
-  //   if (this.get('infinityModelContent.reachedInfinity') && this.get('destroyOnInfinity')) {
-  //     this.destroy();
-  //   }
-  // }),
 
   infinityModelPushed: Ember.observer('infinityModelContent.length', 'viewportEntered', function() {
     if (this.get('viewportEntered')) {
