4622e6d3de6e5c50fe8b8cd9b4a343e3a6d3073f
Greg Larrenaga <greglarrenaga@gmail.com>
2018-01-30 06:50:55 -0800

    Adds hideOnInfinity support to the infinity-loader component (#244)
    * Changes the destroyOnInfinity infinity-loader property to hideOnInfinity
    * When set to true, the infinity-loader will hide itself
    * Adds notes to the readme
    * Adds tests for when the hideOnInfinity is set, and not set.
    closes #243

--- addon/components/infinity-loader.js
+++ addon/components/infinity-loader.js
@@ -9,10 +9,11 @@ const InfinityLoaderComponent = Ember.Component.extend({
   loadMoreAction: 'infinityLoad',
   loadingText: 'Loading Infinite Model...',
   loadedText: 'Infinite Model Entirely Loaded.',
-  destroyOnInfinity: false,
+  hideOnInfinity: false,
   developmentMode: false,
   scrollable: null,
   triggerOffset: 0,
+  isVisible: true,
 
   willInsertElement() {
     if (this.get('_isInfinityPromiseArray')) {
@@ -141,9 +142,9 @@ const InfinityLoaderComponent = Ember.Component.extend({
     }
   },
 
-  loadedStatusDidChange: Ember.observer('infinityModelContent.reachedInfinity', 'destroyOnInfinity', function () {
-    if (this.get('infinityModelContent.reachedInfinity') && this.get('destroyOnInfinity')) {
-      this.destroy();
+  loadedStatusDidChange: Ember.observer('infinityModelContent.reachedInfinity', 'hideOnInfinity', function () {
+    if (this.get('infinityModelContent.reachedInfinity') && this.get('hideOnInfinity')) {
+      this.set('isVisible', false);
     }
   }),
 
