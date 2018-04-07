17709bf8444798cbdd2356227907d567c6cd2d8b
snewcomer <snewcomer24@gmail.com>
2018-04-07 16:56:23 -0700

    fix lint

--- tests/acceptance/infinity-route-test.js
+++ tests/acceptance/infinity-route-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, click, find, triggerEvent, currentURL, waitUntil } from '@ember/test-helpers';
+import { visit, find, triggerEvent, currentURL, waitUntil } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
