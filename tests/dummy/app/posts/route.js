b1f119fcfa99226dc7118e5f3197048f90d9c4d3
snewcomer <snewcomer24@gmail.com>
2018-06-11 11:09:14 -0700

    getter

--- tests/dummy/app/posts/route.js
+++ tests/dummy/app/posts/route.js
@@ -1,10 +1,11 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { get } from '@ember/object';
 
 export default Route.extend({
   infinity: service(),
 
   model() {
-    return this.infinity.model('post', { perPage: 1, startingPage: 1 });
+    return get(this, 'infinity').model('post', { perPage: 1, startingPage: 1 });
   }
 });
