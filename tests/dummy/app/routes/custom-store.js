eeffb218450cedffe3c6949c99d891d29aed1da0
snewcomer <snewcomer24@gmail.com>
2018-06-11 10:07:51 -0700

    minor improvements

--- tests/dummy/app/routes/custom-store.js
+++ tests/dummy/app/routes/custom-store.js
@@ -5,6 +5,7 @@ import { get } from '@ember/object';
 export default Route.extend({
   customStore: service(),
   infinity: service(),
+
   model() {
     return get(this, 'infinity').model('custom-model', { store: get(this, 'customStore'), storeFindMethod: 'findAll' });
   }
