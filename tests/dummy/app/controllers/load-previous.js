3d0f8cf269dbf40efc0385f61e397c0ecc0ef504
Scott Newcomer <snewcomer@users.noreply.github.com>
2018-08-21 18:11:09 -0700

    Update ember-in-viewport to 3.1.1 (#317)

--- tests/dummy/app/controllers/load-previous.js
+++ tests/dummy/app/controllers/load-previous.js
@@ -2,6 +2,6 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   queryParams: ['triggerOffset', 'page'],
-  triggerOffset: 0,
+  triggerOffset: 50,
   page: 1
 });
--- tests/dummy/app/controllers/test-scrollable.js
+++ tests/dummy/app/controllers/test-scrollable.js
@@ -2,7 +2,7 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   queryParams: ['triggerOffset', 'page', 'perPage'],
-  triggerOffset: 0,
+  triggerOffset: 50,
   page: 1,
   perPage: 25
 });
