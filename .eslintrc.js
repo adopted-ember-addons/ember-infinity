ba26636ec757ab710432d196fef2fc3e518a952a
Scott Newcomer <snewcomer@users.noreply.github.com>
2019-01-10 20:50:55 -0800

    Update 3.7 (#357)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,3 @@
-/*eslint-env node*/
 module.exports = {
   root: true,
   parserOptions: {
@@ -21,6 +20,7 @@ module.exports = {
     // node files
     {
       files: [
+        '.eslintrc.js',
         '.template-lintrc.js',
         'ember-cli-build.js',
         'index.js',
