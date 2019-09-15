10fcce1b0ffc2c6bf73796c01a4c0a38884214f7
snewcomer <snewcomer24@gmail.com>
2019-09-14 23:23:10 -0700

    add settled to assertDetails

--- tests/helpers/assert-acceptance-details.js
+++ tests/helpers/assert-acceptance-details.js
@@ -1,9 +1,11 @@
-import { find } from '@ember/test-helpers';
+import { find, settled } from '@ember/test-helpers';
 
-export default function assertDetails(assert, {title, listLength, reachedInfinity}) {
+export default async function assertDetails(assert, {title, listLength, reachedInfinity}) {
   let postsTitle     = find('#posts-title');
   let postList       = find('ul');
   let infinityLoader = find('.infinity-loader');
+
+  await settled();
 
   assert.equal(postsTitle.textContent, title);
   assert.equal(postList.querySelectorAll('li').length, listLength);
