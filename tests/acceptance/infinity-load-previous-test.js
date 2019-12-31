f5c1f02f55c7f4b7a6f1c46643fa0acb75d5d708
snewcomer <snewcomer24@gmail.com>
2019-12-31 13:21:14 -0600

    add await settled in a few spots

--- tests/acceptance/infinity-load-previous-test.js
+++ tests/acceptance/infinity-load-previous-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, find, triggerEvent } from '@ember/test-helpers';
+import { visit, find, settled, triggerEvent } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import defaultScenario from '../../mirage/scenarios/default';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
@@ -48,6 +48,7 @@ module('Acceptance: Infinity Route - load previous', function(hooks) {
     defaultScenario(this.server);
     await visit('/load-previous');
 
+    await settled();
     shouldBeItemsOnTheList(assert, 25);
     infinityShouldNotBeReached(assert);
     scrollTo(triggerOffset() - 100);
@@ -66,6 +67,7 @@ module('Acceptance: Infinity Route - load previous', function(hooks) {
     defaultScenario(this.server);
     await visit('/load-previous?page=2');
 
+    await settled();
     shouldBeItemsOnTheList(assert, 50);
     // This is difficult b/c of #ember-testing-container
     // assert.equal(document.querySelectorAll('.posts p')[25].offsetTop, 12500, 'scrollable list has elements above (each 250px high * 25)');
