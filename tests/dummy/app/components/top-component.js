2a29afba55917549648a47b1e3dc4ab7b42fc7a4
snewcomer <snewcomer24@gmail.com>
2019-12-31 09:01:39 -0600

    refactor one component to class

--- tests/dummy/app/components/top-component.js
+++ tests/dummy/app/components/top-component.js
@@ -1,14 +1,14 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 
-export default Component.extend({
-  infinity: service(),
+export default class TopComponent extends Component {
+  @service infinity
 
-  classNames: ['list-items'],
+  tagName = '';
 
   init() {
-    this._super(...arguments);
+    super.init(...arguments);
 
-    this.posts = this.get('infinity').model('post', { perPage: 5 });
+    this.posts = this.infinity.model('post', { perPage: 5 });
   }
-});
+}
