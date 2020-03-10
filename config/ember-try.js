811fa27b90817a9064bf6da1de6321ba08c89d2f
snewcomer <snewcomer24@gmail.com>
2020-03-09 19:38:35 -0700

    try 3.16 bump

--- config/ember-try.js
+++ config/ember-try.js
@@ -23,6 +23,14 @@ module.exports = async function() {
         }
       },
       {
+        name: 'ember-lts-3.16',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.16.0'
+          }
+        }
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
@@ -66,6 +74,21 @@ module.exports = async function() {
         npm: {
           devDependencies: {
             '@ember/jquery': '^0.5.1'
+          }
+        }
+      },
+      {
+        name: 'ember-classic',
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            'application-template-wrapper': true,
+            'default-async-observers': false,
+            'template-only-glimmer-components': false
+          })
+        },
+        npm: {
+          ember: {
+            edition: 'classic'
           }
         }
       }
