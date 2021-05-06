1fb7995ff93bfa6415b81c6264b0465140413577
Scott Newcomer <snewcomer@users.noreply.github.com>
2021-05-06 10:11:52 -0500

    Fix CI (#449)
    * Fix CI
    
    * add 3.20
    
    * update lockfile
    
    * lint
    
    Co-authored-by: scott-newcomer <snewcomer@apple.com>

--- config/ember-try.js
+++ config/ember-try.js
@@ -23,6 +23,14 @@ module.exports = async function() {
         }
       },
       {
+        name: 'ember-lts-3.20',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.20.0'
+          }
+        }
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
