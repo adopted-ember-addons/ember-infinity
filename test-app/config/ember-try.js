f09d50389c2754df0acb03d20f38c7768ed54286
Adam Woźny <wozny1989@gmail.com>
2024-05-08 15:37:59 +0200

    add scenarios for ember 4.12 and 5.8

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -59,10 +59,26 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-lts-4.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0',
+          },
+        },
+      },
+      {
         name: 'ember-lts-5.4',
         npm: {
           devDependencies: {
             'ember-source': '~5.4.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-5.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.8.0',
           },
         },
       },
